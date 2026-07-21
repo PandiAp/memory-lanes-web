85ba917a9227b237cc9e072ea3e8f42da85593ce
PandiAp <karamelloua@gmail.com>
2026-07-21 12:48:46 +0300

    Connect Supabase project and set live app URL

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.MEMORY_LANES_CONFIG = Object.freeze({
-  supabaseUrl: "",
-  publishableKey: "",
-  appUrl: "",
+  supabaseUrl: "https://lqbxrxsryezlurwgfitb.supabase.co",
+  publishableKey: "sb_publishable_PvGnri2GkeFq7GkImFbs9g_14xN4Kkb",
+  appUrl: "https://pandiap.github.io/memory-lanes-web/",
   allowOwnerSignup: false,
-  allowDemo: true,
+  allowDemo: false,
   pollIntervalMs: 30000
 });
